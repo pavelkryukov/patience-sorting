c741759e6765583cd8fe997f3b4198eb53bb11b5
Pavel I. Kryukov <kryukov@frtk.ru>
2021-02-07 19:32:18 +0300

    Avoid CRTP

--- patience_sort.h
+++ patience_sort.h
@@ -26,77 +26,26 @@
 #include <deque>
 #include <list>
 
-template<typename T>
-class ContiniousDeck
+namespace Patience {
+
+template<typename Storage>
+class Installer
 {
+    using T = typename Storage::value_type::value_type;
 public:
-    template<typename RandomIt>
-    void produce_output(RandomIt begin) noexcept
-    {
-        auto mid = begin;
-        for (auto& deck : storage)
-            mid = merge(deck, begin, mid);
-    }
+    explicit Installer(Storage& storage) : storage(storage) { }
 
-protected:
-    std::deque<std::deque<T>> storage;
-
-private:
-    template<typename RandomIt>
-    static auto merge(std::deque<T>& deck, RandomIt begin, RandomIt mid) noexcept
-    {
-        auto new_mid = std::copy(deck.begin(), deck.end(), mid);
-        std::inplace_merge(begin, mid, new_mid);
-        return new_mid;
-    }
-};
-
-template<typename T>
-class SparceDeck
-{
-public:
-    template<typename RandomIt>
-    void produce_output(RandomIt begin) noexcept
-    {
-        auto result = produce_output();
-        std::move(result.begin(), result.end(), begin);
-    }
-
-    template<typename List>
-    void produce_output_list(List& list) noexcept
-    {
-        list.splice(list.begin(), produce_output());
-    }
-
-protected:
-    std::deque<std::list<T>> storage;
-
-private:
-    auto produce_output() noexcept
-    {
-        std::list<T> result;
-        for (auto& deck : storage)
-            result.merge(deck);
-        
-        return result;
-    }
-};
-
-template<typename T, template<typename> class Deck>
-class DeckFinder : public Deck<T>
-{
-public:
     auto get_deck_pointer(const T& val)
     {
-        auto res = find_deck(val, this->storage.begin(), this->storage.end());
-        return res != this->storage.end() ? res : allocate_new_deck();
+        auto res = find_deck(val, storage.begin(), storage.end());
+        return res != storage.end() ? res : allocate_new_deck();
     }
 
 private:
     auto allocate_new_deck()
     {
-        this->storage.resize(this->storage.size() + 1);
-        return this->storage.end() - 1;
+        storage.resize(storage.size() + 1);
+        return storage.end() - 1;
     }
 
     template<typename DeckIt>
@@ -111,34 +60,102 @@ private:
         auto mid = std::next(begin, std::distance(begin, end) / 2);
         return mid->back() < val ? find_deck(val, begin, mid) : find_deck(val, mid, end);
     }
+    
+    Storage& storage;
+};
+
+template<typename T>
+class ContiniousDeck
+{
+public:
+    auto get_deck_pointer(const T& val)
+    {
+        return Installer(storage).get_deck_pointer(val);
+    }
+
+    template<typename RandomIt>
+    void produce_output(RandomIt begin) noexcept
+    {
+        auto mid = begin;
+        for (auto& deck : storage)
+            mid = merge(deck, begin, mid);
+    }
+
+private:
+    template<typename RandomIt>
+    static auto merge(std::deque<T>& deck, RandomIt begin, RandomIt mid) noexcept
+    {
+        auto new_mid = std::copy(deck.begin(), deck.end(), mid);
+        std::inplace_merge(begin, mid, new_mid);
+        return new_mid;
+    }
+
+    std::deque<std::deque<T>> storage;
+};
+
+template<typename T>
+class SparceDeck
+{
+public:
+    auto get_deck_pointer(const T& val)
+    {
+        return Installer(storage).get_deck_pointer(val);
+    }
+
+    template<typename RandomIt>
+    void produce_output(RandomIt begin) noexcept
+    {
+        auto result = produce_output();
+        std::move(result.begin(), result.end(), begin);
+    }
+
+    template<typename List>
+    void produce_output_list(List& list) noexcept
+    {
+        list.splice(list.begin(), produce_output());
+    }
+
+private:
+    auto produce_output() noexcept
+    {
+        std::list<T> result;
+        for (auto& deck : storage)
+            result.merge(deck);
+        
+        return result;
+    }
+
+    std::deque<std::list<T>> storage;
 };
 
 template<typename RandomIt, template<typename T> class Deck>
-auto patience_sort_generic(RandomIt begin, RandomIt end)
+auto sort_generic(RandomIt begin, RandomIt end)
 {
-    DeckFinder<typename RandomIt::value_type, Deck> deck;
+    Deck<typename RandomIt::value_type> deck;
     for (auto it = begin; it != end; ++it)
         deck.get_deck_pointer(*it)->emplace_back(std::move(*it));
 
     deck.produce_output(begin);
 }
 
+} // namespace Patience
+
 template<typename RandomIt>
 auto patience_sort_cont(RandomIt begin, RandomIt end)
 {
-    patience_sort_generic<RandomIt, ContiniousDeck>(begin, end);
+    Patience::sort_generic<RandomIt, Patience::ContiniousDeck>(begin, end);
 }
 
 template<typename RandomIt>
 auto patience_sort_list(RandomIt begin, RandomIt end)
 {
-    patience_sort_generic<RandomIt, SparceDeck>(begin, end);
+    Patience::sort_generic<RandomIt, Patience::SparceDeck>(begin, end);
 }
 
 template<typename List>
 auto patience_sort(List& list)
 {
-    DeckFinder<typename List::value_type, SparceDeck> deck;
+    Patience::SparceDeck<typename List::value_type> deck;
     for (auto it = list.begin(); it != list.end();) {
         auto tmp = it++;
         auto target = deck.get_deck_pointer(*tmp);
